97f56616e8afeb753a4b9aa7da8eb2ee629b33f9
Greg <gkodikara@gmail.com>
2012-12-24 10:34:51 +1100

    Updates, working table

--- application/controllers/services.php
+++ application/controllers/services.php
@@ -2,13 +2,46 @@
 
 class Services extends CI_Controller {
 
-	public function index() {
+	function __construct() {
+		parent::__construct();
 		$this->load->model("services_model");
-		$this->get_questions();
+		$this->load->library('pagination');
+		$this->load->library("table");
+
 	}
 
-	function get_questions() {
-		var_dump($this->services_model->get_question_list());
+	function index() {
+		$this->get_services();
+	}
+
+	function get_services() {
+		$config['per_page'] = 20; 
+
+		$service_data = $this->services_model->get_services_list($config['per_page'], $this->uri->segment(3));
+
+		$config['base_url'] = 'http://gkodikara.dev/codeigniter/ccc/index.php/services/get_services';
+		$config['total_rows'] = $service_data["total_count"];
+		
+
+		$this->pagination->initialize($config); 
+
+		echo $this->pagination->create_links();
+
+		$this->table->set_heading("Service ID", 
+									"Service Name", 
+									"Service Type", 
+									"Service Location", 
+									"Service Contact", 
+									"Service Contact Telephone"
+									);
+
+		$this->table->set_caption('Available Services');
+
+		$data = array(
+			"services_table" => $this->table->generate($service_data["results"])
+		);
+
+		$this->load->view("services", $data);
 	}
 
 }
--- application/models/services_model.php
+++ application/models/services_model.php
@@ -1,20 +1,26 @@
 <?php 
 	class Services_model extends CI_Model {
 
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
-    	$this->load->database();
-
     }
 
-    function get_question_list() {
-    	$query = $this->db->query('SELECT * FROM questions');
+    function get_services_list($limit, $offset) {
+    	$query = $this->db->query('SELECT * FROM services
+                                    LIMIT ?
+                                    OFFSET ?', array($limit, (int) $offset));
     	$results = array();
-    	foreach ($query->result() as $question) {
-    		$results[] = $question;
+        $total_results_count = $this->db->get("services")->num_rows();
+    	foreach ($query->result() as $service) {
+    		$results[] = (array) $service;
     	}
     	
-    	return $results;
+    	return array(
+            "results" => $results,
+            "total_count" => $total_results_count
+            );
     }
 }
+
+
+?>
