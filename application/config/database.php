e9703496130a06e0976baff6f7a11638f141a004
brijendra tiwari <brijendra@24x7ibuilder.com>
2013-10-04 15:44:59 +0530

    new db.

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '127.0.0.1';
+$db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '558505';
-$db['default']['database'] = 'CCC';
+$db['default']['password'] = '';
+$db['default']['database'] = 'ccc';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
