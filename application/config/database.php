a5d5c676e7250bbd5dd8faa685d8b51519d80e98
root <root@ip-172-31-5-114.ap-southeast-2.compute.internal>
2013-10-10 09:47:03 +0000

    server settings

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '558505';
+$db['default']['password'] = 'crimson55';
 $db['default']['database'] = 'CCC';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
